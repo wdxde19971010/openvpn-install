d92a05b67e68e902f7fe971593cdb15cb7521488
wdxde19971010 <86032747+wdxde19971010@users.noreply.github.com>
2023-07-01 14:23:15 +0800

    Update openvpn-install.sh
    国内服务器可用

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -236,7 +236,7 @@ LimitNPROC=infinity" > /etc/systemd/system/openvpn-server@server.service.d/disab
 		systemctl enable --now firewalld.service
 	fi
 	# Get easy-rsa
-	easy_rsa_url='https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.1/EasyRSA-3.1.1.tgz'
+	easy_rsa_url='http://13.208.164.246:5244/d/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98/gj/EasyRSA-3.1.1.tgz'
 	mkdir -p /etc/openvpn/server/easy-rsa/
 	{ wget -qO- "$easy_rsa_url" 2>/dev/null || curl -sL "$easy_rsa_url" ; } | tar xz -C /etc/openvpn/server/easy-rsa/ --strip-components 1
 	chown -R root:root /etc/openvpn/server/easy-rsa/
